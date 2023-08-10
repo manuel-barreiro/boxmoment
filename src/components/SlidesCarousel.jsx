40b3e13c2b9359c4e2d722e826d31e55fe884104
Manuel Barreiro <manuel.barreiro@ing.austral.edu.ar>
2023-08-10 19:10:52 -0300

    a ver

--- src/components/SlidesCarousel.jsx
+++ src/components/SlidesCarousel.jsx
@@ -4,17 +4,17 @@ import { Carousel } from 'flowbite-react';
 
 const SlidesCarousel = () => {
   return (
-    <Carousel slideInterval={5000} className='h-[20vh] md:h-[60vh] lg:h-[80vh] mt-32'>
-    <img
-      alt="..."
-      src="/images/slides/slide1.png"
-    />
-    <img
-      alt="..."
-      src="/images/slides/slide2.png"
-    />
-    
-  </Carousel>
+    <Carousel className='h-[20vh] md:h-[60vh] lg:h-[80vh] mt-32'>
+      <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
+        <img src="/images/slides/slide1.png" className='object-cover' alt="" />
+      </div>
+      <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
+        Slide 2
+      </div>
+      <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
+        Slide 3
+      </div>
+    </Carousel>
   )
 }
 
