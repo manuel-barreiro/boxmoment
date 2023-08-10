e452138ed74ef42e6631ef6440dd063ba0ccf377
Manuel Barreiro <manuel.barreiro@ing.austral.edu.ar>
2023-08-10 19:27:06 -0300

    carousel

--- src/components/SlidesCarousel.jsx
+++ src/components/SlidesCarousel.jsx
@@ -1,20 +1,29 @@
 import React from 'react'
 
 import { Carousel } from 'flowbite-react';
+import { Flowbite } from 'flowbite-react';
+
+const customTheme = {
+  carousel: {
+    scrollContainer: {
+      "base": "flex h-full snap-mandatory overflow-y-hidden overflow-x-scroll scroll-smooth",
+      "snap": "snap-x"
+    },
+  },
+};
 
 const SlidesCarousel = () => {
   return (
-    <Carousel className='h-[20vh] md:h-[60vh] lg:h-[80vh] mt-32'>
-      <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
-        <img src="/images/slides/slide1.png" className='object-cover' alt="" />
-      </div>
-      <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
-        Slide 2
-      </div>
-      <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
-        Slide 3
-      </div>
-    </Carousel>
+    <Flowbite theme={{ theme: customTheme }}>
+      <Carousel slideInterval={5000} className='h-[20vh] md:h-[60vh] lg:h-[80vh] mt-32'>
+        <div className="flex h-full items-center justify-center bg-pineGreen">
+          <img src="/images/slides/slide1.png" className='object-cover' alt="" />
+        </div>
+        <div className="flex h-full items-center justify-center bg-pineGreen">
+          <img src="/images/slides/slide2.png" className='object-cover' alt="" />
+        </div>
+      </Carousel>
+      </Flowbite>
   )
 }
 
