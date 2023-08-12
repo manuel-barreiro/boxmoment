0be24aeea35d13e789b508ba761812a9eb315a3a
Manuel Barreiro <manuel.barreiro@ing.austral.edu.ar>
2023-08-12 18:02:57 -0300

    client section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar'
 import SlidesCarousel from './components/SlidesCarousel'
 import Products from './components/Products'
 import WhoAreWe from './components/WhoAreWe'
+import Clients from './components/Clients'
 import Experiencia from './components/Experiencia'
 import Footer from './components/Footer'
 import WhatsappButton from './components/WhatsappButton'
@@ -16,6 +17,7 @@ function App() {
     <SlidesCarousel />
     <Products />
     <WhoAreWe />
+    <Clients />
     <Experiencia />
     <Footer/>
     <WhatsappButton />
