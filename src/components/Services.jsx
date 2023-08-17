0b3a3c7707f151092c07e45bb9b5657972983a86
Manuel Barreiro <manuel.barreiro@ing.austral.edu.ar>
2023-08-17 11:53:48 -0300

    correcciones

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -17,11 +17,11 @@ const Services = () => {
             
             <div className="flex relative pt-10 pb-20 sm:items-center md:w-2/3 mx-auto">
             <div className="h-full w-6 absolute inset-0 flex items-center justify-center">
-                <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
+                <div className="h-full w-1 bg-pineGreen/30 pointer-events-none"></div>
             </div>
             <div className="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-pineGreen text-white relative z-10 font-medium text-sm">1</div>
             <div className="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
-                <div className="flex-shrink-0 w-24 h-24 bg-[#f6f6f6] text-pineGreen rounded-full inline-flex items-center justify-center">
+                <div className="flex-shrink-0 w-24 h-24 bg-pineGreen/10  text-pineGreen rounded-full inline-flex items-center justify-center">
                 <AiOutlineShoppingCart size={50} />
                 </div>
                 <div className="flex-grow sm:pl-6 mt-6 sm:mt-0">
@@ -32,11 +32,11 @@ const Services = () => {
             </div>
             <div className="flex relative pb-20 sm:items-center md:w-2/3 mx-auto">
             <div className="h-full w-6 absolute inset-0 flex items-center justify-center">
-                <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
+                <div className="h-full w-1 bg-pineGreen/30 pointer-events-none"></div>
             </div>
             <div className="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-pineGreen text-white relative z-10 font-medium text-sm">2</div>
             <div className="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
-                <div className="flex-shrink-0 w-24 h-24 bg-[#f6f6f6] text-pineGreen rounded-full inline-flex items-center justify-center">
+                <div className="flex-shrink-0 w-24 h-24 bg-pineGreen/10 text-pineGreen rounded-full inline-flex items-center justify-center">
                 <FaBoxOpen size={50} />
                 </div>
                 <div className="flex-grow sm:pl-6 mt-6 sm:mt-0">
@@ -47,11 +47,11 @@ const Services = () => {
             </div>
             <div className="flex relative pb-20 sm:items-center md:w-2/3 mx-auto">
             <div className="h-full w-6 absolute inset-0 flex items-center justify-center">
-                <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
+                <div className="h-full w-1 bg-pineGreen/30  pointer-events-none"></div>
             </div>
             <div className="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-pineGreen text-white relative z-10 font-medium text-sm">3</div>
             <div className="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
-                <div className="flex-shrink-0 w-24 h-24 bg-[#f6f6f6] text-pineGreen rounded-full inline-flex items-center justify-center">
+                <div className="flex-shrink-0 w-24 h-24 bg-pineGreen/10  text-pineGreen rounded-full inline-flex items-center justify-center">
                 <TbTruckDelivery size={50} />
                 </div>
                 <div className="flex-grow sm:pl-6 mt-6 sm:mt-0">
