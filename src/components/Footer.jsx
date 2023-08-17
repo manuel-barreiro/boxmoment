bf31d705bb26af541cc0d73b03b8c36ab9255e31
Manuel Barreiro <manuel.barreiro@ing.austral.edu.ar>
2023-08-17 10:37:08 -0300

    Cajas Navideñas

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer class="text-white body-font">
         <div class="w-full px-10 py-8 mx-auto flex justify-center bg-darkNight items-center sm:flex-row flex-col">
             <img src="images/logoHorizontal.png" className='w-52' alt="" />
-            <p class="text-sm text-white sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4 text-center">Box Moment® - Todos los Derechos Reservados </p>
+            <p class="text-sm text-white sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4 text-center"> ® Box Moment - Todos los Derechos Reservados </p>
         </div>
     </footer>
   )
