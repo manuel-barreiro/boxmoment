afaeebf4e1e296c0b197611e51aff499799d0df7
Manuel Barreiro <manuel.barreiro@ing.austral.edu.ar>
2023-08-22 18:48:55 -0300

    modified who are we section

--- src/components/WhoAreWe.jsx
+++ src/components/WhoAreWe.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import { motion } from 'framer-motion'
+import {useTypewriter, Cursor} from 'react-simple-typewriter';
 
 
 const WhoAreWe = () => {
+
+  //TypeWriter Effect   
+  const [text] = useTypewriter({
+    words: ['Box Moment.', 'dedicación.', 'calidad.', 'servicio.'],
+    loop: {},
+    typeSpeed: 120,
+    deleteSpeed: 140
+  })
 
   const logoAnimationVariants = {
     initial: { y: 15, opacity: 0 },
@@ -17,49 +26,32 @@ const WhoAreWe = () => {
   return (
     <section name='quienesSomos' className="w-full h-full bg-[url('/images/backgrounds/waves1Mobile.svg')] md:bg-[url('/images/backgrounds/waves1.svg')] bg-center bg-cover py-20 md:py-28 px-8 md:px-32 font-primary flex flex-col gap-6">
 
-      <div className="flex flex-col xl:flex-row justify-evenly gap-4">
-          <div className="xl:basis-1/2 h-auto xl:h-80 flex flex-col gap-5 items-start p-6">
-                <motion.h3
-                variants={textAnimationVariants}
-                initial="initial"
-                whileInView="animate"
-                viewport={{ once: true }}
-                transition= {{ duration: 0.1, delay: 0.3}}
-                className="text-3xl md:text-5xl font-extrabold text-center">¿Quiénes somos?</motion.h3>
-                <motion.p 
-                variants={textAnimationVariants}
-                initial="initial"
-                whileInView="animate"
-                viewport={{ once: true }}
-                transition= {{ duration: 0.1, delay: 0.4}}
-                className="text-justify font-secondary font-bold leading-relaxed">Box Moment lleva 4 años de trayectoria en el mercado, brindando la mayor <span className="font-extrabold">comodidad</span> y <span className="font-extrabold">servicio</span> a nuestros clientes en épocas festivas. </motion.p>
-                <motion.p 
-                variants={textAnimationVariants}
-                initial="initial"
-                whileInView="animate"
-                viewport={{ once: true }}
-                transition= {{ duration: 0.1, delay: 0.5}}
-                className="text-justify font-secondary font-bold leading-relaxed">El respaldo de <span className="font-extrabold">empresas de primera línea</span> que eligen nuestros productos son el sello de <span className="font-extrabold">garantía</span> y <span className="font-extrabold">confianza</span> que nos permite llegar a más consumidores de una forma más rápida y segura sin dejar de brindar una <span className="font-extrabold">atención personalizada</span> a cada uno de nuestros clientes.</motion.p>
+      <div className="flex flex-col justify-evenly items-center gap-5 mt-10 p-10 bg-darkNight shadow-2xl rounded-xl text-[#86e498]">
+
+        <div className='flex flex-col xl:flex-row justify-evenly items-center gap-8 '>
+
+            <div className="xl:basis-1/2 h-auto flex flex-col gap-5 items-start ">
+                  <h3 className="text-3xl md:text-4xl font-extrabold text-center">Somos {text}<Cursor/></h3>
+                  <p className="text-justify font-secondary font-medium leading-relaxed">Box Moment lleva 4 años de trayectoria en el mercado, brindando la mayor <span className="font-extrabold">comodidad</span> y <span className="font-extrabold">servicio</span> a nuestros clientes en épocas festivas. </p>
+                  <p className="text-justify font-secondary font-medium leading-relaxed">El respaldo de <span className="font-extrabold">empresas de primera línea</span> que eligen nuestros productos son el sello de <span className="font-extrabold">garantía</span> y <span className="font-extrabold">confianza</span> que nos permite llegar a más consumidores de una forma más rápida y segura sin dejar de brindar una <span className="font-extrabold">atención personalizada</span> a cada uno de nuestros clientes.</p>
+            </div>
+
+            <div className='xl:basis-1/2 h-auto rounded-3xl xl:h-80'>
+              <video controls poster='/poster.png' loading="lazy" className="object-cover object-center w-full h-full rounded-lg" src="/VideoCajas.mp4" title="Somos Box Moment, hacemos las mejores cajas navideñas" alt="Somos Box Moment" />
+            </div>
+
           </div>
 
-          <div className='xl:basis-1/2 h-full rounded-3xl xl:h-80 md:mt-6'>
-            <video controls poster='/poster.png' loading="lazy" className="object-cover object-center w-full h-full" src="/VideoCajas.mp4" title="Somos Box Moment, hacemos las mejores cajas navideñas" alt="Somos Box Moment" />
+          <div>
+            <p className="text-justify font-secondary font-medium leading-relaxed">
+              Nuestras cajas navideñas son una <span className="font-extrabold">excelente</span> opción para los regalos de fin de año que las empresas otorgan a sus colaboradores durante la época festiva. También llegamos a todo consumidor que quiera disfrutar de una mesa dulce acompañada de los<span className="font-extrabold"> mejores productos</span> durante las fiestas, haciendo de ellas noches más ricas.
+              </p>
           </div>
       </div>
 
-      <div>
-        <motion.p 
-        variants={textAnimationVariants}
-        initial="initial"
-        whileInView="animate"
-        viewport={{ once: true }}
-        transition= {{ duration: 0.1, delay: 0.5}} 
-        className="text-justify font-secondary font-bold p-6 -mt-4 leading-relaxed">
-          Nuestras cajas navideñas son una <span className="font-bold">excelente</span> opción para los regalos de fin de año que las empresas otorgan a sus colaboradores durante la época festiva. También llegamos a todo consumidor que quiera disfrutar de una mesa dulce acompañada de los<span className="font-bold"> mejores productos</span> durante las fiestas, haciendo de ellas noches más ricas.
-          </motion.p>
-      </div>
+      
 
-      <div className='flex flex-col gap-8 items-center'>
+      <div className='flex flex-col gap-8 items-center mt-10'>
               
               <h3 className='text-xl md:text-3xl lg:text-4xl font-bold flex gap-4 justify-center items-center text-center'>
                 <hr className='bg-darkNight w-10 md:w-20 lg:w-32 h-1' />
@@ -76,7 +68,7 @@ const WhoAreWe = () => {
                   whileHover={{ scale: 1.1, 
                                 transition: {ease: "linear"}
                         }}
-                  transition= {{ duration: 0.1, delay: 0.3}}
+                  transition= {{ duration: 0.3, delay: 0.4}}
                   className='w-32 md:w-36 lg:w-40'>
                       <img src="/images/clientes/logo-cordis.png" title='Cliente Confianza Box Moment Cajas Navideñas' alt='Cliente Confianza Box Moment Cajas Navideñas' loading='lazy' width='auto' height='auto' />
                   </motion.div>
@@ -88,7 +80,7 @@ const WhoAreWe = () => {
                   whileHover={{ scale: 1.1, 
                                 transition: {ease: "linear"}
                         }}
-                  transition= {{ duration: 0.1, delay: 0.4}}
+                  transition= {{ duration: 0.3, delay: 0.5}}
                   className='w-32 md:w-36 lg:w-40'>
                       <img src="/images/clientes/logoMaxi.png" title='Cliente Confianza Box Moment Cajas Navideñas' alt='Cliente Confianza Box Moment Cajas Navideñas' loading='lazy' width='auto' height='auto' />
                   </motion.div>
@@ -100,7 +92,7 @@ const WhoAreWe = () => {
                   whileHover={{ scale: 1.1, 
                                 transition: {ease: "linear"}
                         }}
-                  transition= {{ duration: 0.1, delay: 0.5}}
+                  transition= {{ duration: 0.3, delay: 0.6}}
                   className='w-32 md:w-36 lg:w-40'>
                       <img src="/images/clientes/gaonaLogo.png" title='Cliente Confianza Box Moment Cajas Navideñas' alt='Cliente Confianza Box Moment Cajas Navideñas' loading='lazy' width='auto' height='auto' />
                   </motion.div>
@@ -112,7 +104,7 @@ const WhoAreWe = () => {
                   whileHover={{ scale: 1.1, 
                                 transition: {ease: "linear"}
                         }}
-                  transition= {{ duration: 0.1, delay: 0.6}}
+                  transition= {{ duration: 0.3, delay: 0.7}}
                   className='w-32 md:w-36 lg:w-40'>
                       <img src="/images/clientes/logoRIS.png" title='Cliente Confianza Box Moment Cajas Navideñas' alt='Cliente Confianza Box Moment Cajas Navideñas' loading='lazy' width='auto' height='auto' />
                   </motion.div>
